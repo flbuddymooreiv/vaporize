b7b0b78759fc3168919e43d66b0a01d93a3cee70
Buddy Moore <buddy@gmail.com>
2020-01-23 18:48:45 -0500

    Order targets around mouse position when invoked

--- vaporize.py
+++ vaporize.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-import os
-import sys
 import math
 import pyautogui
 from PIL import Image
-import random
 
-targets = int(sys.argv[1])
 ss = pyautogui.screenshot()
 ss = ss.crop((0,0,1672,919))
 ss.save('ss.png')
@@ -57,22 +53,18 @@ def findnxns(region, n):
     
 
 size = 1
-reds = []
 
 while findnxn(ss,size): 
     size *= 2
-    os.system('notify-send -t 1000 "Finding targets: ' + str(size) + '"')
 
 reds = []
-while len(reds) < targets and size > 0:
+while size > 1: #1 because dividing by 2 will never get us to 0
     size /= 2
     corners, ss = findnxns(ss,math.ceil(size))
     reds += [(x+size/2, y+size/2) for (x,y) in corners]
 
-os.system('notify-send "Randomizing targets"')
-#random.shuffle(reds)
-#for r in reds[0:targets]: pyautogui.click (x = r [0], y = r [1])
-def dist(p): return math.sqrt(math.pow(p[0] - pos[0], 2) + math.pow(p[1] - pos[1], 2))
-reds = sorted(reds, key=dist)
-#print(reds)
-for r in reds: pyautogui.click (x = r [0], y = r [1])
+def dist(p1, p2): return math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2))
+def distpos(p): return dist(p, pos)
+reds = sorted(reds, key=distpos)
+for r in reds:
+    pyautogui.click(x = r[0], y= r[1])
