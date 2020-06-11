ce37e2f31bcbe75bdb999bc66db8691e19aff21e
flameSla <39356103+flameSla@users.noreply.github.com>
2020-06-11 19:02:48 +0700

    Update vaporize.py
    accelerated functions:
       def findnxn_new(region, n): 
       def findnxns(region, n):

--- vaporize.py
+++ vaporize.py
@@ -12,49 +12,63 @@ pos = pyautogui.mouseinfo.position()
 def enoughred(p):
     return p[0] >= 96 and p[1] < 32 and p[2] < 32
 
-def findnxn(region, n):
-    for i in range(0,math.floor(region.width)-1):
-        for j in range(0,math.floor(region.height)-1):
+def is_the_square_red(region, i, j, n): 
+    #as soon as we find a non red pixel we immediately exit the loop
+    try:
+        for x in range(1,n):
+            for y in range(1,n):
+                if not enoughred(region.getpixel((i+x,j+y))):
+                    return False				
+    except IndexError as e:
+        return False
+    return True
+           
+i = 0
+j = 0
+first_square_found = dict()
+def findnxn_new(region, n):
+    #we remember the coordinates of the previously found square and continue searching for these coordinates
+    global i, j, first_square_found
+    while i < math.floor(region.width)-1:
+        while j < math.floor(region.height)-1:
             p = region.getpixel((i,j))
             if enoughred(p):
-                search=[]
-                try:
-                    for x in range(0,n):
-                        for y in range(0,n):
-                            p = region.getpixel((i+x,j+y))
-                            search += [p]
-                    if all([enoughred(p) for p in search]):
-                        return True
-                except IndexError as e:
-                    pass
+                if is_the_square_red(region, i, j, n):
+                    first_square_found[n] = [ i,j ]
+                    return True
+            j += 1
+        i += 1
+        j = 0
     return False
 
 def findnxns(region, n):
     corners = []
     region = region.copy()
-    for i in range(0,math.floor(region.width)-1):
-        for j in range(0,math.floor(region.height)-1):
+    #we continue the search with the coordinates that we have memorized in findnxn_new(region, n)
+    i = first_square_found[n][0] 
+    j = first_square_found[n][1]
+    while i < math.floor(region.width)-1:
+        while j < math.floor(region.height)-1:
             p = region.getpixel((i,j))
             if enoughred(p):
                 try:
-                    search=[]
-                    for x in range(0,n):
-                        for y in range(0,n):
-                            p = region.getpixel((i+x,j+y))
-                            search += [p]
-                    if all([enoughred(p) for p in search]):
-                        corners += [[i,j]]
-                        for x in range(0,n):
+                    if is_the_square_red(region, i, j, n): 
+                        #fill in the rectangle
+                        corners += [[int(i+n*7/5/2),int(j+n/2)]]
+                        for x in range(0, int(n*7/5) ):
                             for y in range(0,n):
                                 region.putpixel((i+x,j+y), (0,0,0))
                 except IndexError as e:
                     pass
+            j += 1
+        i += 1
+        j = 0
     return corners, region
-    
+
 
 size = 1
 
-while findnxn(ss,size): 
+while findnxn_new(ss,size): 
     size *= 2
 
 orgreds = []
